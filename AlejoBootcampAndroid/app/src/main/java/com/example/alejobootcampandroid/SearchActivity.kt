dc7bf63eeb80fe92346af259fe68766f060e16c1
alejopp <alejo51120@gmail.com>
2022-05-23 08:28:40 -0500

    Search activity created

---
+++ AlejoBootcampAndroid/app/src/main/java/com/example/alejobootcampandroid/SearchActivity.kt
@@ -0,0 +1,63 @@
+package com.example.alejobootcampandroid
+
+import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SearchView
+import androidx.core.view.MenuItemCompat
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.alejobootcampandroid.databinding.ActivitySearchBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+
+class SearchActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySearchBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySearchBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Add Toolbar
+       // setSupportActionBar(findViewById(R.id.toolbar))
+
+        val navView: BottomNavigationView = binding.navView
+
+        val navController = findNavController(R.id.nav_host_fragment_activity_search)
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home, R.id.navigation_search, R.id.navigation_play
+            )
+        )
+        setupActionBarWithNavController(navController, appBarConfiguration)
+        navView.setupWithNavController(navController)
+    }
+
+/*    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater = menuInflater
+        inflater.inflate(R.menu.top_bar_menu, menu)
+        val searchItem = menu?.findItem(R.id.action_search)
+        val searchView = searchItem?.actionView as SearchView
+        searchView.setQueryHint(getText(R.string.action_search));
+        searchView.setOnQueryTextListener(SearchView.OnQueryTextListener(){
+             fun onQueryTextSubmit(query:String):Boolean{
+                Toast.makeText(this, "Submited", Toast.LENGTH_SHORT).show();
+                //se oculta el EditText
+                searchView.setQuery("", false);
+                searchView.setIconified(true);
+                return true;
+            }
+        })
+        return super.onCreateOptionsMenu(menu)
+
+    }*/
+
+
+}
